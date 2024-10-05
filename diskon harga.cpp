edd769ce8ce69062366a475d1b18126105da18b7
edrickkkeren <163963460+edrickkkeren@users.noreply.github.com>
2024-10-05 23:03:13 +0700

    Add files via upload

---
+++ diskon harga.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+    long hargamentah, harga;
+    cout<<"Penentu Diskon Harga"<<endl;
+    cout<<"Silahkan masukkan total belanja: "<<endl;
+    cin>>hargamentah;
+    harga = (0.1 * hargamentah);
+    cout<<"Diskon mu adalah : "<<harga<<endl;
+    return 0;
+
+}
